849b9188152ba2f0c9db19d05ab56eb34e9b0a35
KISHLAY568 <kishlayk568@gmail.com>
2024-01-29 12:41:32 +0530

    first commit

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const q = require('lodash');
-const numbers = [1,2,3,4,5];
-const doubledNumbers = [1,2,3,4,5];
-console.log('original numbers:',numbers);
-console.log('Doubled numbers:',doubledNumbers);
+const a = require('./add.js');
+const numbers = [1, 2, 3, 4, 5];
+const doubledNumbers = [1, 2, 3, 4, 5];
+console.log('original numbers:', numbers);
+console.log('Doubled numbers:', doubledNumbers);
+let p = a.add(2, 4);
+console.log(p);
