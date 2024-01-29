fd7b337cbbf0606c746a104a799cd737c4cf1121
KISHLAY568 <kishlayk568@gmail.com>
2024-01-29 12:31:56 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const q = require('lodash');
+const numbers = [1,2,3,4,5];
+const doubledNumbers = [1,2,3,4,5];
+console.log('original numbers:',numbers);
+console.log('Doubled numbers:',doubledNumbers);
