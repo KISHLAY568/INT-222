63e88176859ef3070b638d5ab64afd9a549cfa4e
KISHLAY568 <kishlayk568@gmail.com>
2024-02-22 13:52:28 +0530

    first commit

---
+++ EXPRESS/readFile.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const fs = require("fs");
+app.get("/readfile", (req, res) => {
+  const filePath = "example.txt";
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      return res.status(500).send("Error reading the file");
+    }
+
+    res.send(data);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
