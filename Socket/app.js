7d587e43454f1657b30f1256116226e7f554a0c6
KISHLAY568 <kishlayk568@gmail.com>
2024-03-14 12:54:03 +0530

    first commit

---
+++ Socket/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const socketIO = require("socket.io");
+const path = require("path");
+const app = express();
+
+const server = app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
+
+const io = socketIO(server);
+
+app.use(express.static(path.join(__dirname)));
+
+io.on("connection", (socket) => {
+  console.log("A user connected to the server");
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
