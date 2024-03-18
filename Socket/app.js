b147f0d2ff418e5e9df96b084e373ab10dbc0594
KISHLAY568 <kishlayk568@gmail.com>
2024-03-18 13:16:46 +0530

    first commit

--- Socket/app.js
+++ Socket/app.js
@@ -14,10 +14,28 @@ app.use(express.static(path.join(__dirname)));
 io.on("connection", (socket) => {
   console.log("A user connected to the server");
 
+  socket.on("join", (username) => {
+    socket.username = username;
+    io.emit("chat message", {
+      type: "notification",
+      message: `${username} has joined the chat`,
+    });
+  });
+
   socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
+    io.emit("chat message", {
+      type: "message",
+      username: socket.username,
+      message: msg,
+    });
   });
   socket.on("disconnect", () => {
     console.log("A user disconnected");
+    if (socket.username) {
+      io.emit("chat message", {
+        type: "notification",
+        message: `${socket.username} has left the chat`,
+      });
+    }
   });
 });
